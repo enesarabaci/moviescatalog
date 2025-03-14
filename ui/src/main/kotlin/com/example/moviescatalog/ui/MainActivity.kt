4544492ec054c335c8f124fe6fb23f40792e532a
Enes <enes.arabaci@digiturk.com.tr>
2025-03-14 17:11:34 +0300

    themes and colors created and app color palette adjusted

--- ui/src/main/kotlin/com/example/moviescatalog/ui/MainActivity.kt
+++ ui/src/main/kotlin/com/example/moviescatalog/ui/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge() // TODO
-
         setContentView(binding.root)
     }
 }
