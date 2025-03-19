75fe95f182c68d364150ca93f76038296296a99b
Enes <enes.arabaci@digiturk.com.tr>
2025-03-20 00:31:33 +0300

    okhttpclient cache mechanizm integrated

--- network/src/main/kotlin/com/example/moviescatalog/network/di/NetworkModule.kt
+++ network/src/main/kotlin/com/example/moviescatalog/network/di/NetworkModule.kt
@@ -12,9 +12,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.Date
 import javax.inject.Singleton
 
@@ -35,6 +37,12 @@ object NetworkModule {
         headerInterceptor: HeaderInterceptor
     ): OkHttpClient {
         return OkHttpClient.Builder()
+            .cache(
+                Cache(
+                    directory = File(applicationContext.cacheDir, "http_cache"),
+                    maxSize = 50L * 1024L * 1024L
+                )
+            )
             .addNetworkInterceptor(headerInterceptor)
             .addNetworkInterceptor(ChuckerInterceptor(applicationContext))
             .build()
